e15cb127ac94d83c7b3b69d37f24b5bf3419ae32
Rajat <rajatrana541@gmail.com>
2025-03-30 11:17:47 +0530

    comments

---
+++ src/learnjava/comments.java
@@ -0,0 +1,8 @@
+package learnjava;
+
+public class comments {
+    public static void main(String[] args) {
+        // single line comment
+        System.out.println("single line comment");
+    }
+}
