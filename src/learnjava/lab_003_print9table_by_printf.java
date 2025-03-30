162c9f6e003e487084e1ecd848babeabbc6458de
Rajat <rajatrana541@gmail.com>
2025-03-30 22:46:24 +0530

    9 table by using prinf command

---
+++ src/learnjava/lab_003_print9table_by_printf.java
@@ -0,0 +1,18 @@
+package learnjava;
+
+public class lab_003_print9table_by_printf {
+    public static void main(String[] args) {
+        int a = 9 , b = 18, c = 27, d = 36, e = 45, f = 54, g = 63, h = 72, i = 81, j = 90;
+        System.out.printf("9*1 = %d\n",a);
+        System.out.printf("9*2 = %d\n",b);
+        System.out.printf("9*3 = %d\n",c);
+        System.out.printf("9*4 = %d\n",d);
+        System.out.printf("9*5 = %d\n",e);
+        System.out.printf("9*6 = %d\n",f);
+        System.out.printf("9*7 = %d\n",g);
+        System.out.printf("9*8 = %d\n",h);
+        System.out.printf("9*9 = %d\n",i);
+        System.out.printf("9*10 = %d\n",j);
+
+    }
+}
