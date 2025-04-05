15dbc27a8e42d8c21d6d0eb4f4bb0d6e8d3d9aeb
Rajat <rajatrana541@gmail.com>
2025-04-05 23:02:14 +0530

    13 march class task

--- src/Increment_decreament/Task_increment.java
+++ src/Increment_decreament/Task_increment.java
@@ -4,7 +4,11 @@ public class Task_increment {
     public static void main(String[] args) {
         int a = 10;
         System.out.println(++a + a++ + a++);
+        // 11 + 12 + 13
+        // addition will be  11 + 11 + 12  = 34
+
         System.out.println(a);
+        //a = 13
 
     }
 }
