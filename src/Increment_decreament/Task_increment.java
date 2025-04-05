a79af345b85a46ec4264708805f4aae44a53c2a5
Rajat <rajatrana541@gmail.com>
2025-04-05 22:59:13 +0530

    13 march class task

---
+++ src/Increment_decreament/Task_increment.java
@@ -0,0 +1,10 @@
+package Increment_decreament;
+
+public class Task_increment {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+
+    }
+}
