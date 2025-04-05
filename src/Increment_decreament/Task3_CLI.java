7fc0bd502a044925f21241552d440ec6e7e945e1
Rajat <rajatrana541@gmail.com>
2025-04-06 00:22:52 +0530

    13 march class task4  max no

--- src/Increment_decreament/Task3_CLI.java
+++ src/Increment_decreament/Task3_CLI.java
@@ -2,6 +2,7 @@ package Increment_decreament;
 
 public class Task3_CLI {
     public static void main(String[] args) {
+      //Take a user input - Name, Age and Salary and print them in the end.
        String Name = args[0];
        int Age =Integer.parseInt(args[1]);
         int Salary =Integer.parseInt(args[2]);
---
+++ src/Increment_decreament/Task_4.java
@@ -0,0 +1,18 @@
+package Increment_decreament;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        //Take a user input 2 numbers from the arguments and calculate the maximum in between with ternary operator.
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int result = Math.max(num1, num2);
+        System.out.println("max no is = "+result);
+
+
+
+
+
+    }
+}
